4bd498c44378c8b08a7b44c4d52c700a5a98e5b0
Shiven <24969290+shivenspatel@users.noreply.github.com>
2023-01-03 18:31:16 -0600

    Inital Code Push Correction

--- cal_generator.py
+++ cal_generator.py
@@ -73,30 +73,3 @@ class event():
         ical_event['dtstamp'] = ical.vDatetime(datetime.datetime.utcnow())
 
         return ical_event
-
-# def main():
-#     ics = calendar()
-
-#     one_day = event()
-#     one_day.single_day(7, 4, 2023, "One Day")
-#     x = one_day.build()
-
-#     ics.add_event(x)
-
-#     time_task = event()
-#     time_task.time_event(8, 4, 2023, 15, 0, 16, 0, "America/Indianapolis", "Time Event")
-#     y = time_task.build()
-
-#     ics.add_duration(60)
-#     ics.add_event(y)
-
-#     multi_event = event()
-#     multi_event.multi_day(9, 4, 2023, 13, 4, 2023, "Multi Day")
-    
-#     z = multi_event.build()
-    
-#     ics.add_event(z)
-    
-#     ics.build_file('test')
-
-# main()
